1d2d1b91d16910135963f415299481c21f436603
Hilst <hilstfelipe@gmail.com>
2022-07-23 11:47:13 -0300

    update table creation

--- database/sql/create_table.sql
+++ database/sql/create_table.sql
@@ -1,17 +1,17 @@
 
 CREATE TABLE IF NOT EXISTS users (
   userId INT NOT NULL PRIMARY KEY,
-  nome VARCHAR(250) NOT NULL,
+  nickname VARCHAR(250) NOT NULL,
   email VARCHAR(250) NOT NULL,
-  senha CHAR(60) NOT NULL
+  psw CHAR(60) NOT NULL
 );
 
-INSERT INTO users (userId, nome, email, senha)
+INSERT INTO users (userId, nickname, email, psw)
 VALUES (1, 'admin', 'admin@email.com', '$2a$14$STkstinmQGYsPPo04EKBd.yhsI8.qY0qiB3rsBwaU7o4HVuerieku')
 ON CONFLICT DO NOTHING;
 -- admin123
 
-INSERT INTO users (userId, nome, email, senha)
+INSERT INTO users (userId, nickname, email, psw)
 VALUES (2, 'hilst', 'hilst@email.com', '$2a$14$Gk/Eoh4D..8zC7okpmdfMOvrdekydiLBOIWanVn7VjMYr6MigQeRu')
 ON CONFLICT DO NOTHING;
 -- hilst0987
